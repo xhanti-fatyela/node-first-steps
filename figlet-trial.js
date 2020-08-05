02dbbf592f3dae5d15f9a617461f7ea631d0b4c5
xhanti-fatyela <xmanmrcool@gmail.com>
2020-08-05 12:12:15 +0200

    initial commit

--- figlet-trial.js
+++ figlet-trial.js
@@ -1,6 +1,10 @@
 var figlet = require('figlet');
  
-figlet('Hello World!!', function(data) {
-  
+figlet('Hello, Xola', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
     console.log(data)
 });
